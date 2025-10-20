f2574e55ff848b239d64934a4dca7a4d3853cc8d
Bella Roberson <162742673+brobers0339@users.noreply.github.com>
2025-10-20 20:44:10 +0000

    Exercise 1 completed with docstring comments added for get_term_dict() function

--- apputil.py
+++ apputil.py
@@ -8,12 +8,34 @@ class MarkovText(object):
         self.term_dict = None  # you'll need to build this
 
     def get_term_dict(self):
+        '''
+        Build a term dictionary mapping each word in the corpus to a list of words 
+        that immediately follow it.
 
-        # your code here ...
+        This function splits the corpus text into individual words, then iterates 
+        through them to create a mapping where each key is a word and its value 
+        is a list of words that occur directly after that word in the corpus.
 
-        self.term_dict = {}
+        Returns
+        -------
+        dict
+            A dictionary where keys are words (str) and values are lists of 
+            subsequent words (list of str).
+        '''
 
-        return None
+        self.corpus = self.corpus.split()
+
+        self.term_dict = defaultdict(list)
+
+        index = 1
+
+
+        for word in self.corpus:
+            if index < len(self.corpus) - 1:
+                self.term_dict[word].append(self.corpus[index])
+                index += 1
+            else:
+                return self.term_dict
 
 
     def generate(self, seed_term=None, term_count=15):
